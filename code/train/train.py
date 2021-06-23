06310ab94e54e744980d76f1ee68b0db91fb463f
pym7857 <ympark4@ls-electric.com>
2021-06-23 14:10:44 +0900

    patch: train.py

--- code/train/train.py
+++ code/train/train.py
@@ -1,130 +1,53 @@
-import os
-import argparse
-import itertools
+#%%
 import numpy as np
 import pandas as pd
-import joblib
 import matplotlib.pyplot as plt
 
-from sklearn import datasets
-from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+#%%
+data = pd.read_csv("https://github.com/data-labs/data/raw/main/weight-height.csv")
+
+# 단위 변환 (인치, 파운드 --> cm, kg)
+data.Height = data.Height*2.54 # cm
+data.Weight = data.Weight*0.453592 # kg
+
+# 사본 사용 (원본 백업)
+df = data.copy()
+
+#%%
 from sklearn.model_selection import train_test_split
+x = df.Height.values
+y = df.Weight.values
 
-from azureml.core import Dataset, Run
-run = Run.get_context()
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
+#%%
+# 학습 및 검증 데이터 크기 확인
+print(x_train.shape, x_test.shape, y_train.shape,y_test.shape)
 
-def log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix', title='Confusion matrix', cmap=plt.cm.Blues):
-    '''
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    '''
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis = 1)[:, np.newaxis]
-        print('Normalized confusion matrix')
-    else:
-        print('Confusion matrix, without normalization')
-    print(cm)
-    
-    plt.figure()
-    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(labels))
-    plt.xticks(tick_marks, labels, rotation = 45)
-    plt.yticks(tick_marks, labels)
-    
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment = "center", color = 'white' if cm[i, j] > thresh else 'black')
-    
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
-    run.log_image(log_name, plot=plt)
-    plt.savefig(os.path.join('outputs', '{0}.png'.format(log_name)))
+#%%
+# 모델 생성, 학습, 사용 후 성능 보기 (R-Squared)
+from sklearn.linear_model import LinearRegression
+model = LinearRegression() # (1) 모델 생성
+model.fit(x_train.reshape(-1,1), y_train) # (2) 학습
+print(model.score(x_test.reshape(-1,1), y_test)) # (3) 이용, 성능평가
 
+#%%
+# 학습한 계수(파라미터)
+a, b = model.coef_, model.intercept_
+print(a, b)
 
-def log_confusion_matrix(cm, labels):
-    # log confusion matrix as object
-    cm_json =   {
-       'schema_type': 'confusion_matrix',
-       'schema_version': 'v1',
-       'data': {
-           'class_labels': labels,
-           'matrix': cm.tolist()
-       }
-    }
-    run.log_confusion_matrix('confusion_matrix', cm_json)
-    
-    # log confusion matrix as image
-    log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix_unnormalized', title='Confusion matrix')
-    
-    # log normalized confusion matrix as image
-    log_confusion_matrix_image(cm, labels, normalize=True, log_name='confusion_matrix_normalized', title='Normalized confusion matrix')
+#%%
+# 훈련과 검증 데이터에 대한 회귀 직선 보기
+plt.figure(figsize=(10,4))
+xs = np.linspace(140,200,2)
+ys = a*xs + b
 
+plt.subplot(1,2,1)  
+plt.title('Train Data')
+plt.plot(xs,ys, c='r')
+plt.scatter(x_train,y_train, s=0.05)
 
-def main(args):
-    # create the outputs folder
-    os.makedirs('outputs', exist_ok=True)
-    
-    # Log arguments
-    run.log('Kernel type', np.str(args.kernel))
-    run.log('Penalty', np.float(args.penalty))
-
-    # Load iris dataset
-    X, y = datasets.load_iris(return_X_y=True)
-    
-    #dividing X,y into train and test data
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=223)
-    data = {'train': {'X': x_train, 'y': y_train},
-            'test': {'X': x_test, 'y': y_test}}
-    
-    # train a SVM classifier
-    svm_model = SVC(kernel=args.kernel, C=args.penalty, gamma='scale').fit(data['train']['X'], data['train']['y'])
-    svm_predictions = svm_model.predict(data['test']['X'])
-
-    # accuracy for X_test
-    accuracy = svm_model.score(data['test']['X'], data['test']['y'])
-    print('Accuracy of SVM classifier on test set: {:.2f}'.format(accuracy))
-    run.log('Accuracy', np.float(accuracy))
-    
-    # precision for X_test
-    precision = precision_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('Precision of SVM classifier on test set: {:.2f}'.format(precision))
-    run.log('precision', precision)
-    
-    # recall for X_test
-    recall = recall_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('Recall of SVM classifier on test set: {:.2f}'.format(recall))
-    run.log('recall', recall)
-    
-    # f1-score for X_test
-    f1 = f1_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('F1-Score of SVM classifier on test set: {:.2f}'.format(f1))
-    run.log('f1-score', f1)
-    
-    # create a confusion matrix
-    labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-    labels_numbers = [0, 1, 2]
-    cm = confusion_matrix(y_test, svm_predictions, labels_numbers)
-    log_confusion_matrix(cm, labels)
-    
-    # files saved in the "outputs" folder are automatically uploaded into run history
-    model_file_name = "model.pkl"
-    joblib.dump(svm_model, os.path.join('outputs', model_file_name))
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--kernel', type=str, default='rbf', help='Kernel type to be used in the algorithm')
-    parser.add_argument('--penalty', type=float, default=1.0, help='Penalty parameter of the error term')
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    main(args=args)
+plt.subplot(1,2,2)  
+plt.title('Test Data')
+plt.plot(xs,ys, c='r')
+plt.scatter(x_test,y_test, s=0.05)
